82d5aeb6e73592cdb3aa6d7126d92553edef9aa1
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-23 15:19:33 +0300

    feat: created variables for color used in the app

---
+++ lib/core/theme/color.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const Color kAppBarBgColor = Color(0xFF2196F3);
+const Color kPrimaryColor = Color(0xFF1565C0);
+const Color kSecondaryColor = Color(0xFF292929);
+const Color kDangerColor = Color(0xFFEB1607);
+const Color kScaffoldBgLightColor = Color(0xFFF1EEEE);
+const Color kScaffoldBgDarkColor = Color(0xFF000000);
+const Color kIconLightColor = Color(0xFFFFFFFF);
+const Color kIconDarkColor = Color(0xFF000000);
+const Color kIconSecondaryColor = Color(0xFF757575);
+const Color kTextLightColor = Color(0xFFFFFFFF);
+const Color kTextSecondaryColor = Color(0xFF78909C);
+const Color kTextDarkColor = Color(0xFF000000);
+const Color kTextBlueColor = Color(0xFF1565C0);
+const Color kTextFormFieldLabelColor = Color(0xFF1565C0);
+const Color kButtonDarkColor = Color(0xFF1565C0);
+const Color kUserOnlineBadgeColor = Color(0xFF04F554);
