e5eb15d119101162b4f2969e580a2d757f77243a
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-24 11:34:31 +0300

    feat: created a method to get the latest messages

--- lib/data/models/chat.dart
+++ lib/data/models/chat.dart
@@ -67,8 +67,13 @@ class ChatMessage {
   int get hashCode {
     return id.hashCode ^ text.hashCode ^ isMe.hashCode ^ timestamp.hashCode;
   }
-}
 
+  // Get latest message from a list of messages
+  ChatMessage getLatestMessage(List<ChatMessage> messages) {
+    messages.sort((a, b) => b.timestamp.compareTo(a.timestamp));
+    return messages.first;
+  }
+}
 
 List<ChatMessage> messages = [
   ChatMessage(
