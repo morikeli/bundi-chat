bda966869d3234c0dca189e02d511189c7356c4c
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-26 13:19:26 +0300

    feat: created a util object for the loading indicators

---
+++ lib/core/utils/app_loading_indicators.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+import '../theme/color.dart';
+
+class AppLoadingIndicators {
+  static Widget loadingIndicatorExtraSmall() {
+    return LoadingAnimationWidget.threeRotatingDots(
+      color: kIconLightColor,
+      size: 12.0,
+    );
+  }
+
+  static Widget loadingIndicatorSmall() {
+    return LoadingAnimationWidget.threeRotatingDots(
+      color: kIconLightColor,
+      size: 16.0,
+    );
+  }
+
+  static Widget loadingIndicatorMedium() {
+    return LoadingAnimationWidget.threeRotatingDots(
+      color: kIconLightColor,
+      size: 24.0,
+    );
+  }
+
+  static Widget loadingIndicatorLarge() {
+    return LoadingAnimationWidget.threeRotatingDots(
+      color: kIconLightColor,
+      size: 32.0,
+    );
+  }
+}
