292a5004237370f3d6760e8eff5dc2e7811521ac
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-26 13:25:19 +0300

    feat: appended routes for login and signup screens in routes

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../presentation/views/auth/login_screen.dart';
+import '../presentation/views/auth/signup_screen.dart';
 import '../presentation/views/chat/chat_screen.dart';
 import '../presentation/views/home/homescreen.dart';
 
 final Map<String, WidgetBuilder> routes = {
+  LoginScreen.routeName: (context) => const LoginScreen(),
+  SignupScreen.routeName: (context) => const SignupScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
   ChatScreen.routeName: (context) => const ChatScreen(),
 
