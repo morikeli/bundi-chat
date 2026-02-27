5e70310a13d64b3d20a7f97ffc467d9c6b5fa975
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-27 08:03:30 +0300

    feat: updated the `ChatMessage` with new attrs

--- lib/data/models/chat.dart
+++ lib/data/models/chat.dart
@@ -1,28 +1,36 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 class ChatMessage {
   final String id;
   final String text;
-  final bool isMe;
+  final String senderId;
+  final String receiverId;
   final bool isRead;
   final DateTime timestamp;
 
   const ChatMessage({
     required this.id,
     required this.text,
-    required this.isMe,
+    required this.senderId,
     required this.timestamp,
     required this.isRead,
+    required this.receiverId,
   });
+
+  bool get isSentByMe => senderId == Supabase.instance.client.auth.currentUser?.id;
 
   ChatMessage copyWith({
     String? id,
     String? text,
-    bool? isMe,
+    String? senderId,
+    String? receiverId,
     DateTime? timestamp,
   }) {
     return ChatMessage(
       id: id ?? this.id,
       text: text ?? this.text,
-      isMe: isMe ?? this.isMe,
+      senderId: senderId ?? this.senderId,
+      receiverId: receiverId ?? this.receiverId,
       timestamp: timestamp ?? this.timestamp,
       isRead: isRead,
     );
@@ -32,7 +40,7 @@ class ChatMessage {
     return {
       'id': id,
       'text': text,
-      'isMe': isMe,
+      'senderId': senderId,
       'timestamp': timestamp.millisecondsSinceEpoch,
     };
   }
@@ -41,7 +49,8 @@ class ChatMessage {
     return ChatMessage(
       id: json['id'],
       text: json['text'],
-      isMe: json['isMe'],
+      senderId: json['senderId'],
+      receiverId: json['receiverId'],
       timestamp: DateTime.fromMillisecondsSinceEpoch(json['timestamp']),
       isRead: json['isRead'],
     );
@@ -49,7 +58,7 @@ class ChatMessage {
 
   @override
   String toString() {
-    return '''ChatMessage(id: $id, text: $text, isMe: $isMe, timestamp: $timestamp)''';
+    return '''ChatMessage(id: $id, text: $text, senderId: $senderId, receiverId: $receiverId, timestamp: $timestamp)''';
   }
 
   @override
@@ -59,43 +68,48 @@ class ChatMessage {
     return other is ChatMessage &&
         other.id == id &&
         other.text == text &&
-        other.isMe == isMe &&
+        other.senderId == senderId &&
+        other.receiverId == receiverId &&
         other.timestamp == timestamp;
   }
 
   @override
   int get hashCode {
-    return id.hashCode ^ text.hashCode ^ isMe.hashCode ^ timestamp.hashCode;
+    return id.hashCode ^ text.hashCode ^ senderId.hashCode ^ timestamp.hashCode;
   }
 }
 
-List<ChatMessage> messages = [
-  ChatMessage(
-    id: '1',
-    text: 'Hello, how are you?',
-    isMe: false,
-    timestamp: DateTime.now().subtract(Duration(minutes: 5)),
-    isRead: false,
-  ),
-  ChatMessage(
-    id: '2',
-    text: 'I am good, thanks! How about you?',
-    isMe: true,
-    timestamp: DateTime.now().subtract(Duration(minutes: 4)),
-    isRead: true,
-  ),
-  ChatMessage(
-    id: '3',
-    text: 'I am doing well too. What are you up to?',
-    isMe: false,
-    timestamp: DateTime.now().subtract(Duration(minutes: 3)),
-    isRead: false,
-  ),
-  ChatMessage(
-    id: '4',
-    text: 'Just working on a Flutter project.',
-    isMe: true,
-    timestamp: DateTime.now().subtract(Duration(minutes: 2)),
-    isRead: false,
-  ),
-];
+// List<ChatMessage> messages = [
+//   ChatMessage(
+//     id: '1',
+//     text: 'Hello, how are you?',
+//     senderId: 'user1',
+//     receiverId: 'user2',
+//     timestamp: DateTime.now().subtract(Duration(minutes: 5)),
+//     isRead: false,
+//   ),
+//   ChatMessage(
+//     id: '2',
+//     text: 'I am good, thanks! How about you?',
+//     senderId: 'user2',
+//     receiverId: 'user1',
+//     timestamp: DateTime.now().subtract(Duration(minutes: 4)),
+//     isRead: true,
+//   ),
+//   ChatMessage(
+//     id: '3',
+//     text: 'I am doing well too. What are you up to?',
+//     senderId: 'user1',
+//     receiverId: 'user2',
+//     timestamp: DateTime.now().subtract(Duration(minutes: 3)),
+//     isRead: false,
+//   ),
+//   ChatMessage(
+//     id: '4',
+//     text: 'Just working on a Flutter project.',
+//     senderId: 'user2',
+//     receiverId: 'user1',
+//     timestamp: DateTime.now().subtract(Duration(minutes: 2)),
+//     isRead: false,
+//   ),
+// ];
