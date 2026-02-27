9cbc23ef65f3af3f3edc78649d462b1a959b967f
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-02-27 10:14:14 +0300

    feat: appended `ChatBloc` to the BLoC provider

--- lib/core/providers/bloc_providers.dart
+++ lib/core/providers/bloc_providers.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../data/repository/auth_repository.dart';
+import '../../data/repository/chat_repository.dart';
 import '../../presentation/bloc/auth_bloc/auth_bloc.dart';
+import '../../presentation/bloc/chat_bloc/chat_bloc.dart';
 
 /// Provides Blocs used across the app.
 class AppBlocProviders extends StatelessWidget {
@@ -15,6 +17,9 @@ class AppBlocProviders extends StatelessWidget {
       providers: [
         BlocProvider(
           create: (context) => AuthBloc(context.read<AuthRepository>()),
+        ),
+        BlocProvider(
+          create: (context) => ChatBloc(context.read<ChatRepository>()),
         ),
       ],
       child: child,
