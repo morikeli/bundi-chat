574500ab1d7e064ec9c9d85aca7a897c83a7fdff
magnus.prime <78599959+morikeli@users.noreply.github.com>
2026-03-06 08:32:54 +0300

    feat: appended a new routeName for the `StartNewChatScreen()`

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../presentation/views/auth/login_screen.dart';
 import '../presentation/views/auth/signup_screen.dart';
 import '../presentation/views/chat/chat_screen.dart';
+import '../presentation/views/chat/start_new_chat_screen.dart';
 import '../presentation/views/home/homescreen.dart';
 
 final Map<String, WidgetBuilder> routes = {
@@ -10,5 +11,5 @@ final Map<String, WidgetBuilder> routes = {
   SignupScreen.routeName: (context) => const SignupScreen(),
   HomeScreen.routeName: (context) => const HomeScreen(),
   ChatScreen.routeName: (context) => const ChatScreen(),
-
+  StartNewChatScreen.routeName: (context) => const StartNewChatScreen(),
 };
